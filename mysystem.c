71ccedc3662af811bc023e8329b281b6a388ab2f
i21nakamura <i21nakamura@tokuyama.kosen-ac.jp>
2024-07-04 12:03:51 +0900

    提出1回目

--- mysystem.c
+++ mysystem.c
@@ -11,14 +11,56 @@
 // system関数のクローン
 int mysystem(char *command) {
   int status = 0;
-
-  // ここにプログラムを書く
-
+  int pid;
+  if (command == NULL) {                              //NULLポインタを渡された場合
+    return 1;                                         //1を返す
+  }
+  if ((pid = fork()) < 0) {                           //分身を作る
+    return -1;                                        //forkできなかった場合は-1を返す
+  }
+  if (pid != 0) {                                     //親プロセス
+    while (wait(&status) != pid)                      //子プロセスの終了を待つ
+      ;
+  } else {                                            //子プロセス
+    execl("/bin/sh", "/bin/sh", "-c", command, NULL); //shを実行
+    exit(127);                                        //エラー終了
+  }
   return status;
 }
 
 /* 実行例
-
-ここに実行例を書く
-
+$ make
+cc -Wall -std=c99 -o mysysmain mysysmain.c  mysystem.c     <-- コンパイル時エラーなし
+$ ls -l                                                    <-- コマンドラインで実行した結果
+total 292
+-rw-r--r-- 1 hina    143  7  4 09:43 Makefile
+-rw-r--r-- 1 hina   2795  7  4 09:43 README.md
+-rw-r--r-- 1 hina 238232  7  4 09:43 README.pdf
+-rwxr-xr-x 1 hina  33816  7  4 10:08 mysysmain*
+-rw-r--r-- 1 hina    925  7  4 09:43 mysysmain.c
+-rw-r--r-- 1 hina    879  7  4 10:07 mysystem.c
+-rw-r--r-- 1 hina     90  7  4 09:43 mysystem.h
+$ ./mysysmain "ls -l"
+mysystem:                                                  <-- mysistemの結果
+total 584
+-rw-r--r--  1 hina  staff     143  7  4 09:43 Makefile
+-rw-r--r--  1 hina  staff    2795  7  4 09:43 README.md
+-rw-r--r--  1 hina  staff  238232  7  4 09:43 README.pdf
+-rwxr-xr-x  1 hina  staff   33816  7  4 10:11 mysysmain
+-rw-r--r--  1 hina  staff     925  7  4 09:43 mysysmain.c
+-rw-r--r--  1 hina  staff     894  7  4 10:11 mysystem.c
+-rw-r--r--  1 hina  staff      90  7  4 09:43 mysystem.h
+retval = 00000000
+system:                                                    <-- systemの結果
+total 584
+-rw-r--r--  1 hina  staff     143  7  4 09:43 Makefile
+-rw-r--r--  1 hina  staff    2795  7  4 09:43 README.md
+-rw-r--r--  1 hina  staff  238232  7  4 09:43 README.pdf
+-rwxr-xr-x  1 hina  staff   33816  7  4 10:11 mysysmain
+-rw-r--r--  1 hina  staff     925  7  4 09:43 mysysmain.c
+-rw-r--r--  1 hina  staff     894  7  4 10:11 mysystem.c
+-rw-r--r--  1 hina  staff      90  7  4 09:43 mysystem.h
+retval = 00000000
+$ ./mysysmain                                              <-- 引数なしの場合      
+使い方 : ./mysysmain コマンド文字列                            <-- Usageメッセージを表示
 */
